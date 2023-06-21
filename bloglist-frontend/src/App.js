3388cd458352411cfd835f819ee1d3a1710ff17d
Ashvith Shetty <113123021+Ashvith10@users.noreply.github.com>
2023-06-21 13:51:56 +0530

    Complete step 10 for blog list frontend

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -126,7 +126,9 @@ const App = () => {
                         />
                     </Toggleable>
                     <div id="blogs">
-                        {blogs.map(blog => <Blog key={blog.id} blog={blog} setBlogs={setBlogs}/>)}
+                        {blogs
+                            .sort((a, b) => b.likes - a.likes)
+                            .map(blog => <Blog key={blog.id} blog={blog} setBlogs={setBlogs}/>)}
                     </div>
                 </div>
         }
