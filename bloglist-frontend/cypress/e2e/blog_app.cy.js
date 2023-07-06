de8855588351b02b680f54d96f7596b19073722c
Ashvith Shetty <113123021+Ashvith10@users.noreply.github.com>
2023-07-07 01:35:22 +0530

    bloglist end to end testing, step4

--- bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,10 +1,10 @@
 import chaiColors from 'chai-colors'
-chai.use(chaiColors);
+chai.use(chaiColors)
 
 const credentials = {
-  "username": "username",
-  "name": "Name",
-  "password": "password"
+  'username': 'username',
+  'name': 'Name',
+  'password': 'password'
 }
 
 describe('template spec', () => {
@@ -25,7 +25,7 @@ describe('template spec', () => {
     beforeEach(() => {
       cy.request('POST', 'http://localhost:3003/api/users', credentials)
     })
-    
+
     it('succeeds with correct credentials', () => {
       cy.get('.username').type(credentials.username)
       cy.get('.password').type(credentials.password)
@@ -39,8 +39,8 @@ describe('template spec', () => {
 
     it('fails with wrong credentials', () => {
       const rngStrGenerator = () => Math.random()
-                                      .toString(36)
-                                      .substring(2)
+        .toString(36)
+        .substring(2)
 
       cy.get('.username').type(rngStrGenerator())
       cy.get('.password').type(rngStrGenerator())
@@ -55,11 +55,11 @@ describe('template spec', () => {
 
   describe('When logged in', () => {
     const blog = {
-      "title": 'Title of the Blog',
-      "author": "John·Doe",
-      "url": "https://www.example.com/"
+      'title': 'Title of the Blog',
+      'author': 'John·Doe',
+      'url': 'https://www.example.com/'
     }
-    
+
     beforeEach(() => {
       cy.request('POST', 'http://localhost:3003/api/users', credentials)
       cy.get('.username').type(credentials.username)
@@ -80,6 +80,29 @@ describe('template spec', () => {
       cy.get('.blog-author').contains(blog.author)
       cy.get('.show').click()
       cy.get('.blog-url').contains(blog.url)
+    })
+
+    describe('On creation', () => {
+      beforeEach(() => {
+        cy.get('.affirm').click()
+
+        cy.get('.blog-title-field').type(blog.title)
+        cy.get('.blog-author-field').type(blog.author)
+        cy.get('.blog-url-field').type(blog.url)
+
+        cy.get('.createNote').click()
+      })
+
+      it('A blog can be liked', () => {
+        cy.get('.show').click()
+
+        cy.get('.blog-likes').then(element => {
+          const prevVal = element.text()
+          cy.get('.like').click()
+          const newVal = element.text()
+          expect(newVal).to.eq(prevVal)
+        })
+      })
     })
   })
 })
