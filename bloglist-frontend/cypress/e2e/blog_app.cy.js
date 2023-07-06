166786fc522601ce315851389c664906ee124ab4
Ashvith Shetty <113123021+Ashvith10@users.noreply.github.com>
2023-07-06 19:20:32 +0530

    bloglist end to end testing, step2

--- bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,8 +1,9 @@
+import chaiColors from 'chai-colors'
+chai.use(chaiColors);
+
 describe('template spec', () => {
   beforeEach(() => {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
-    cy.request('POST', 'http://localhost:3003/api/users',
-      { "username": "username","name": "Name","password": "password" })
     cy.visit('http://localhost:3000/')
   })
 
@@ -12,5 +13,43 @@ describe('template spec', () => {
     cy.get('.username').should('exist')
     cy.get('.password').should('exist')
     cy.get('.submitLogin').should('exist')
+  })
+
+  describe('Login', () => {
+    const credentials = {
+      "username": "username",
+      "name": "Name",
+      "password": "password"
+    }
+
+    beforeEach(() => {
+      cy.request('POST', 'http://localhost:3003/api/users', credentials)
+    })
+    
+    it('succeeds with correct credentials', () => {
+      cy.get('.username').type(credentials.username)
+      cy.get('.password').type(credentials.password)
+      cy.get('.submitLogin').click()
+
+      cy.get('.successMessage')
+        .contains('Login succeeded')
+        .should('have.css', 'color')
+        .and('be.colored', '#008000')
+    })
+
+    it('fails with wrong credentials', () => {
+      const rngStrGenerator = () => Math.random()
+                                      .toString(36)
+                                      .substring(2)
+
+      cy.get('.username').type(rngStrGenerator())
+      cy.get('.password').type(rngStrGenerator())
+      cy.get('.submitLogin').click()
+
+      cy.get('.errorMessage')
+        .contains('Invalid username and password')
+        .should('have.css', 'color')
+        .and('be.colored', '#ff0000')
+    })
   })
 })
